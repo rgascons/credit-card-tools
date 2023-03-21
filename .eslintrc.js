6666a12eda833c4e24006ccf7bc2a8e6c1c0d3c0
Ricard Gascons <gascons1995@gmail.com>
2023-03-20 18:43:14 -0700

    add eslint and tsconfig

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['airbnb-base', 'airbnb-typescript/base'],
+  overrides: [
+  ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+  }
+}
